aeeeaa27e51a54d7fe587e793ab64e4ba484a0ad
Nils Ponsard <nilsponsard@gmail.com>
2023-01-25 21:58:03 +0100

    feat: markdown rendering

---
+++ routes/blog/[...slug].tsx
@@ -0,0 +1,42 @@
+import { Handlers, PageProps } from '$fresh/server.ts';
+import { Head } from '$fresh/runtime.ts';
+import { gfm } from '../../utils/markdown.ts';
+
+interface Data {
+  markdown: string;
+  slug: string;
+}
+
+export const handler: Handlers<Data> = {
+  GET(req, ctx) {
+    const slug = ctx.params.slug;
+    const markdown = `
+# Hello World
+
+test
+    `;
+    const resp = ctx.render({ slug, markdown });
+
+    return resp;
+  },
+};
+
+export default function BlogPost(props: PageProps<Data>) {
+  const { slug, markdown } = props.data;
+
+  const renderedMarkdown = gfm.render(markdown);
+  return (
+    <>
+      <Head>
+        <link rel="stylesheet" href={`/gfm.css?build=${__FRSH_BUILD_ID}`} />
+      </Head>
+      <div class="p-4 mx-auto max-w-screen-md m">
+        <h1>{slug}</h1>
+        <div
+          class="mt-6 markdown-body"
+          dangerouslySetInnerHTML={{ __html: renderedMarkdown }}
+        />
+      </div>
+    </>
+  );
+}
---
+++ utils/markdown.ts
@@ -0,0 +1,7 @@
+export * as gfm from "https://deno.land/x/gfm@0.1.28/mod.ts";
+import "https://esm.sh/prismjs@1.27.0/components/prism-jsx.js?no-check";
+import "https://esm.sh/prismjs@1.27.0/components/prism-typescript.js?no-check";
+import "https://esm.sh/prismjs@1.27.0/components/prism-tsx.js?no-check";
+import "https://esm.sh/prismjs@1.27.0/components/prism-diff.js?no-check";
+
+export { extract as  frontMatter } from "https://deno.land/std@0.173.0/encoding/front_matter/any.ts";
